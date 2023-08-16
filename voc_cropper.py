96b18474ac13759a5cf3f223c20d1ceecd01f969
Yongjik Kim <yongjik.kim2@gmail.com>
2023-08-16 09:43:27 +0900

    cropper fix and duplicate scan feature added

--- voc_cropper.py
+++ voc_cropper.py
@@ -6,6 +6,7 @@ import sys
 import argparse
 import xml.etree.ElementTree as ET
 from PIL import Image
+import imagehash
 
 def extract_annotation(obj_name, xml_path):    
 
@@ -42,8 +43,6 @@ def save_crops(current_path, img_name, img_ext_str, annotations):
     image_path = current_path+img_name+img_ext_str
     image = Image.open(image_path)
     for i in range(len(annotations)):
-        if (annotation['ymax']-annotation['ymin']<25):
-            continue
         annotation = annotations[i]
         crop_path = current_path + 'cropped/' + img_name + '_{}'.format(i) + img_ext_str
         cropped_image = image.crop((annotation['xmin'], annotation['ymin'], 
@@ -60,7 +59,30 @@ def main(args):
                 xml_file = file.replace(img_ext_str, '.xml')
                 xml_path = args.root_dir + '/Annotations/' + xml_file
                 annotation = extract_annotation(args.obj_name, xml_path)
-                save_crops(path, img_name, img_ext_str, annotation)
+                save_annot = []
+                for i in range(len(annotation)):
+                    if ((annotation[i]['ymax'] - annotation[i]['ymin']>30)
+                        and (annotation[i]['xmax'] - annotation[i]['xmin'])>30):
+                        save_annot.append(annotation[i])
+                save_crops(path, img_name, img_ext_str, save_annot)
+        break
+
+    if args.dupe_scan:
+        print("Cropping done, looking for near-identical images and deleting them ...")
+        cropped_path = args.root_dir + '/JPEGImages/' + 'cropped/'
+        hashes = {}
+        for _, _, files in os.walk(cropped_path):
+            for file in files:
+                if img_ext_str in file:
+                    image = Image.open(cropped_path+file)
+                    h = imagehash.average_hash(image)
+                    if h in hashes:
+                        print(f"Duplicate found: {file} and {hashes[h]}")
+                        os.remove(cropped_path+file)  # Uncomment to actually delete the file
+                    else:
+                        hashes[h] = file
+            break
+        print("Near-identical images deleted. Cropper exiting with success.")
 
 def parse_arguments(argv):
 
@@ -72,6 +94,8 @@ def parse_arguments(argv):
                         help='name of object to be cropped', default = 'license_plate')
     parser.add_argument('--img_ext', type=str,
                         help='extension of image files', default = 'jpg')
+    parser.add_argument('--dupe_scan', type=bool,
+                        help='looks for duplicated crops and deletes them', default = False)
 
     return parser.parse_args(argv)
 
